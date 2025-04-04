8cd3dcac9c9bd64a65a778f2cb932fa6c24ff6f2
Kamogelo Motau <127486770+motau-kamogelo@users.noreply.github.com>
2025-04-04 09:37:52 +0200

    Update MainActivity.kt

--- app/src/main/java/com/example/myhero/MainActivity.kt
+++ app/src/main/java/com/example/myhero/MainActivity.kt
@@ -21,7 +21,7 @@ class MainActivity : ComponentActivity() {
             MyHeroTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
                     Greeting(
-                        name = "Android 11 for Us",
+                        name = "Android 11 for us SA Citizens",
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
